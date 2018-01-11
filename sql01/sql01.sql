03d2f8d7e18fc22c39183ed20bdc029cd06e17f8
BIT-USER <BIT-USER@DESKTOP-GO4E25Q>
2018-01-11 16:36:24 +0900

    01/10 2nd

--- sql01/sql01.sql
+++ sql01/sql01.sql
@@ -3,7 +3,7 @@
 선임부터 출력이 되도록 하세요. 이름(first_name last_name),  월급(salary),  전화번호
 phone_number), 입사일(hire_date) 순서이고 “이름”, “월급”, “전화번호”, “입사일” 
 로 컬럼이름을 대체해 보세요.*/
-select first_name || last_name "이름", 
+select first_name || ' ' || last_name "이름", 
         salary "월급", 
         phone_number "전화번호", 
         hire_date "입사일"
@@ -13,7 +13,8 @@ order by hire_date asc;
 /*문제2.
 업무(jobs)별로 업무이름(job_title)과 최고월급(max_salary)을 월급의 내림차순(DESC)로 정
 렬하세요.*/
-select job_title, max_salary
+select job_title, 
+		max_salary
 from jobs
 order by max_salary desc;
 
